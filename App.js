a02857aa9f0977aead11a545003b384b3a9978e3
Connor Zanin <cnnrznn@gmail.com>
2023-10-09 20:31:20 -0600

    Move functionality into the form. Grab location and add lat+lon to callback object

--- App.js
+++ App.js
@@ -1,16 +1,8 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Form from './components/playform';
 import Games from './components/game-list';
-import { v4 as uuid } from 'uuid';
-import { useState } from 'react';
 
 export default function App() {
-  const [id, setID] = useState();
-
-  function changeUUID() {
-    console.log("old ID: ", id);
-    setID(uuid());
-  }
 
   function formCallback(obj) {
     console.log(obj);
@@ -18,9 +10,6 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Play together!</Text>
-      <Text>ID: {id}</Text>
-      <Button onPress={changeUUID} title='Change UUID'/>
       <Form callback={formCallback} />
       <Games />
     </View>
--- components/playform.js
+++ components/playform.js
@@ -1,24 +1,61 @@
 import { StyleSheet, View, Text, Button } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { useState } from 'react';
-
+import Geolocation from '@react-native-community/geolocation';
+import {v4 as uuid} from 'uuid';
 
 
 const Form = (props) => {
     const submitCallback = props.callback;
 
+    const [id, setID] = useState(uuid());
+
     const [selectedActivity, setSelectedActivity] = useState("volleyball");
     const [selectedTime, setSelectedTime] = useState(60);
+
+    const [lat, setLat] = useState();
+    const [lon, setLon] = useState();
+
+    // this is here for demo purposes
+    // in a release version a UUID would be generated once
+    // and stored on-device
+    function changeUUID() {
+        setID(uuid());
+    }
 
     function submitForm() {
         submitCallback({
             time: selectedTime,
             activity: selectedActivity,
+            latitude: lat,
+            longitude: lon,
         });
+    }
+
+    function GetLocation() {
+        Geolocation.getCurrentPosition(
+            (position) => {
+            console.log(position);
+            setLat(position.coords.latitude);
+            setLon(position.coords.longitude);
+            },
+            (error) => {
+            console.log(error);
+            }
+        );
     }
 
     return (
         <View>
+            <View style={styles.row}>
+                <Text>Lat: {lat}</Text>
+                <Text>Lon: {lon}</Text>
+                <Button onPress={GetLocation} title="Get Locatin" />
+            </View>
+            <View style={styles.row}>
+                <Text>ID: {id}</Text>
+                <Button onPress={changeUUID} title='Change UUID'/>
+            </View>
             <View style={styles.row} >
                 <Text style={styles.col}>Activity:</Text>
                 <Picker
@@ -35,6 +72,8 @@ const Form = (props) => {
                     onValueChange={(time, index) => setSelectedTime(time)} >
                     <Picker.Item label="1 hour" value={60} />
                     <Picker.Item label="2 hours" value={120} />
+                    <Picker.Item label="3 hours" value={180} />
+                    <Picker.Item label="4 hours" value={240} />
                 </Picker>
             </View>
             <Button 
